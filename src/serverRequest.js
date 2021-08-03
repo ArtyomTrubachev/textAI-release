4dd443951ba87e86058030d155caac0a435d9822
ArtyomTrubachev <artyom.trubachev@gmail.com>
2021-08-04 00:32:30 +0300

    change Server JS

--- src/serverRequest.js
+++ src/serverRequest.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
         request.open('POST', 'http://35.243.225.189:8080/cgi-bin/script.cgi', true)
 
         request.addEventListener('readystatechange', function () {
-            console.log(parseInt(request.responseText.replace(/[^(\d+(.\d+)?)]/g, '')));
+            console.log(parseInt(request.responseText.replace(/[^\d]/g, '')));
             document.querySelector("#img").classList.add('opacity');
             document.querySelector("#result").innerHTML = `${parseInt(request.responseText.replace(/[^\d]/g, ''))} % unique`;
         })

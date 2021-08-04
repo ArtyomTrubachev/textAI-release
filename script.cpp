837876f5e5ff2dff5c805cf4d7fd4df45ba1af80
ArtyomTrubachev <artyom.trubachev@gmail.com>
2021-08-04 19:35:10 +0300

    Work version

--- script.cpp
+++ script.cpp
@@ -52,7 +52,7 @@ int main()
     
     name = form("name");
     if (!name.empty()) {
-    	cout << round(anitPlagiarism(getDB(), name))<< "\n";
+    	cout << (anitPlagiarism(getDB(), name)<< "\n";
     } else {
     	cout << "Text is not provided!\n";
     }	
